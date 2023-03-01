98004a0e703de6af618d1ec2098c551050b1f49d
Day Little <dayana.y.little@gmail.com>
2023-03-01 00:48:42 -0500

    write handle function for acct deletion and create delete button

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,3 +1,6 @@
+// services
+import * as authService from '../../services/authService'
+
 // stylesheets
 import styles from './Landing.module.css'
 
@@ -6,14 +9,27 @@ import { User } from '../../types/models'
 
 interface LandingProps {
   user: User | null;
+  handleLogout: () => void;
 }
 
-const Landing = (props: LandingProps): JSX.Element => {
-  const { user } = props
+const Landing = ({ user, handleLogout }: LandingProps): JSX.Element => {
+
+
+  const handleDeleteAccount = async (): Promise<void> => {
+    await authService.deleteAccount()
+    handleLogout()
+  }
 
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Meow Meow <br /> Beans</h1>
+      {/* <img src={} alt="A meow meow bean" /> */}
+
+      {user &&
+        <button onClick={handleDeleteAccount}>
+          DELETE ACCOUNT
+        </button>
+      }
     </main>
   )
 }
